6a5757a18224f3dbafa859885ba9c3207ce317c5
Henintsoa <henintsoacr7@gmail.com>
2019-10-24 16:37:53 +0200

    Master commit test

--- watermelon/src/Body.js
+++ watermelon/src/Body.js
@@ -2,12 +2,68 @@ import React, { Component } from 'react';
 import { Button } from 'reactstrap';
 import './App.css';
 import { isLogicalExpression } from '@babel/types';
+import signOut from './body/signOut.js';
 
-class Body extends Component{
-    render(){
-        return(
+class Body extends Component {
+    constructor(props) {
+        super(props);
+        this.displayBody = this.displayBody.bind(this);
+    }
+
+    displayBody() {
+
+        switch (this.props.page) {
+            case 'signOut':
+                return signOut();
+
+            case 'addCard':
+
+                break;
+
+            case 'delCard':
+
+                break;
+
+            case 'updCard':
+
+                break;
+
+            case 'deposit':
+
+                break;
+
+            case 'withdrawal':
+
+                break;
+
+            case 'transfer':
+
+                break;
+
+            case 'account':
+
+                break;
+
+            case 'signUp':
+
+                break;
+
+            case 'signIn':
+
+                break;
+
+            default:
+                alert('default case');
+                break;
+        }
+    }
+
+    render() {
+        return (
             <main class="container-fluid" role="main">
-                
+                {this.displayBody()}
+                page: {this.props.page} <br />
+                user: {this.props.user}
             </main>
         );
 
